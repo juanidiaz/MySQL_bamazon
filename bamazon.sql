be108b8782e222ccdb37b4db4f7ae8331f83ac97
juanidiaz <jdi@idiaz.ca>
2019-02-11 23:54:49 -0500

    Challenge #1: Customer View (Minimum Requirement)

---
+++ bamazon.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS bamazon;
+CREATE DATABASE bamazon;
+
+USE bamazon;
+
+CREATE TABLE products(
+  item_id INT NOT NULL AUTO_INCREMENT,
+  product_name VARCHAR(100) NOT NULL,
+  department_name VARCHAR(100) NOT NULL,
+  price DECIMAL(10,2) default 0,
+  stock_quantity DECIMAL(10,2) default 0,
+  PRIMARY KEY (item_id)
+);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Fire 7 Tablet, 7in Display, 8 GB, Black", "Electronics", 59.99, 310), ("All-new Echo Dot (3rd gen)", "Electronics", 69.99, 324), ("Apple Watch Series 3", "Electronics", 499.99, 120);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Fisher-Price On-The-Go Baby Dome", "Baby", 97.01, 140), ("ECO by Naty Baby Travel Wipes", "Baby", 39.8, 387), ("Boba Baby Wrap, Gray", "Baby", 55, 420);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Toastess TSG710 Sandwich Grill", "Appliances", 39.96, 5), ("Oster Belgian Waffle Maker", "Appliances", 31.54, 78), ("Cuisinart CPK-17C Programmable Kettle", "Appliances", 99.99, 104);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Levoit Air Purifier", "Home", 19.92, 167), ("ACTCUT Super Soft Indoor Modern Shag", "Home", 39.99, 245), ("Zinus Memory Foam 12 Inch Green Tea Mattress", "Home", 259, 5);
+
+SELECT * FROM products;
---
+++ bamazonCustomer.js
@@ -0,0 +1,121 @@
+// Defining dependencies
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var dash = "\n**************************************\n"
+
+var connection = mysql.createConnection({
+  // Address to SQL server
+  host: "localhost",
+
+  // Port to SQL server
+  port: 3306,
+
+  // username
+  user: "root",
+
+  // password
+  password: "root",
+
+  // DB name
+  database: "bamazon"
+});
+
+// Create connection to DB
+connection.connect(function (err) {
+  if (err) throw err; // If there are any errors throw the error
+
+  // If everything is ok then start the CLI application
+  start();
+});
+
+function start() {
+  // Query the database for all items on sale
+  connection.query("SELECT * FROM products", function (err, results) {
+    if (err) throw err;
+
+    inquirer
+      .prompt([{
+          name: "itemToBuy",
+          type: "rawlist",
+          message: "What do you want to buy today?",
+          choices: function () {
+
+            // Once you have the items display them for the user to choose
+            var itemsArray = [];
+            for (var i = 0; i < results.length; i++) {
+              itemsArray.push(results[i].product_name + "  CDN $" + results[i].price);
+            }
+            //console.log(itemsArray);
+            return itemsArray;
+          }
+        },
+        {
+          name: "qty",
+          type: "input",
+          message: "How many units you want to buy?"
+        },
+      ])
+      .then(function (answer) {
+
+        // Get the information of the chosen item
+        var chosenItem;
+        for (var i = 0; i < results.length; i++) {
+          if ((results[i].product_name + "  CDN $" + results[i].price) === answer.itemToBuy) {
+            chosenItem = results[i];
+            //console.log(chosenItem);
+          };
+        };
+
+        // Check if there is enough stock
+        if ((chosenItem.stock_quantity - parseInt(answer.qty)) > 0) {
+
+          // If there is enough stock to clear the order then provide price and reduce stock
+          connection.query(
+            "UPDATE products SET ? WHERE ?",
+            [{
+                stock_quantity: (chosenItem.stock_quantity - parseInt(answer.qty))
+              },
+              {
+                item_id: chosenItem.item_id
+              }
+            ],
+            function (error) {
+              if (error) throw err;
+              console.log(dash + "Order placed successfully!\nYour total is CDN$" + (chosenItem.price * parseInt(answer.qty)) + dash);
+
+              // Once order was placed give option to keep buying or quit
+              var message = "Would you like to keep buying?"
+              again(message);
+
+            }
+          );
+        } else {
+          // If there is not enough stock, allow user to reduce quantity or quit
+          var message = "Not enough inventory to clear your order. Do you want to try again with a lower quantity?"
+          again(message);
+
+        }
+      });
+  });
+};
+
+function again(msg) {
+  inquirer
+    .prompt({
+      name: "again",
+      type: "confirm",
+      message: msg,
+    })
+    .then(function (answer) {
+      if (answer.again) {
+
+        // Start again the buying cycle
+        start();
+      } else {
+
+        // close connection and quit
+        connection.end();
+      }
+    });
+};
